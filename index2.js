fb938b2deaf3dd1297eb1ef486b6b7c8cb1a4488
Nefesa <nefesa@hotmail.com>
2020-02-14 14:24:21 -0600

    New2

--- index2.js
+++ index2.js
@@ -5,5 +5,9 @@ let name1 =("My name is")
 let name2 =("Nefesa")
 console.log =("")
 
-let = prompt ("What is your balance")
+let  balance = prompt ("What is your balance")
+let balance = prompt ("Your new balance")
+let = (10)
+alert 
+
 
